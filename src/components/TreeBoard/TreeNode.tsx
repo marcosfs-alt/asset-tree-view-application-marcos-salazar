02f71ec2834f884129c26012497c17ba2c885d3c
Marcos Salazar <marcos1010521@gmail.com>
2024-10-09 09:35:54 -0400

    chore: wrap treenode in memo

--- src/components/TreeBoard/TreeNode.tsx
+++ src/components/TreeBoard/TreeNode.tsx
@@ -1,84 +1,86 @@
-'use client';
-
 import { Items, TreeNodeProp, sensorStatus, sensorTypes } from '@/types';
 import Image from 'next/image';
 import ArrowDown from '@/../../public/assets/icons/arrowDown.svg';
 import EnergyIcon from '@/../../public/assets/icons/bolt.svg';
 import ComponentIcon from '@/../../public/assets/icons/component.svg';
-import { useState } from 'react';
+import { useState, memo } from 'react';
 
-const TreeNode = ({
-  name,
-  children,
-  isLeaf,
-  onClick,
-  selected,
-  type,
-  sensorType,
-  status,
-}: TreeNodeProp) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+const TreeNode = memo(
+  ({
+    name,
+    children,
+    isLeaf,
+    onClick,
+    selected,
+    type,
+    sensorType,
+    status,
+  }: TreeNodeProp) => {
+    const [isExpanded, setIsExpanded] = useState(false);
 
-  const toggleExpanded = () => {
-    if (!isLeaf) {
-      setIsExpanded((curr) => !curr);
-    }
-  };
+    const toggleExpanded = () => {
+      if (!isLeaf) {
+        setIsExpanded((curr) => !curr);
+      }
+    };
 
-  const handleClick = () => {
-    if (isLeaf && onClick) {
-      onClick();
-      return;
-    }
-    toggleExpanded();
-  };
+    const handleClick = () => {
+      if (isLeaf && onClick) {
+        onClick();
+        return;
+      }
+      toggleExpanded();
+    };
 
-  return (
-    <div className={`pl-2 ${selected ? 'bg-blue500 text-white' : ''}`}>
-      <div
-        className="cursor-pointer flex gap-[4px] flex items-center"
-        onClick={handleClick}
-      >
-        {!isLeaf ? (
-          isExpanded ? (
-            <ArrowDown />
+    return (
+      <div className={`pl-2 ${selected ? 'bg-blue500 text-white' : ''}`}>
+        <div
+          className="cursor-pointer flex gap-[4px] items-center"
+          onClick={handleClick}
+        >
+          {!isLeaf ? (
+            isExpanded ? (
+              <ArrowDown />
+            ) : (
+              <ArrowDown className="rotate-[270deg]" />
+            )
           ) : (
-            <ArrowDown className="rotate-[270deg]" />
-          )
-        ) : (
-          ''
-        )}
-        {type === Items.CPT ? (
-          <ComponentIcon
-            className={`${!selected ? 'fill-blue500' : 'fill-white'}`}
-            width={22}
-            height={22}
-          />
-        ) : (
-          <Image
-            src={`/assets/icons/${type}.svg`}
-            alt="blue color icon"
-            className={`${!selected ? 'fill-white' : 'fill-blue500'}`}
-            width={22}
-            height={22}
-          />
-        )}
-        {name}
-        {isLeaf &&
-          !!sensorType &&
-          (sensorType === sensorTypes.E ? (
-            <EnergyIcon
-              className={`filter ${status === sensorStatus.OPR ? 'fill-defaultGreen' : 'fill-defaultRed'}`}
+            ''
+          )}
+          {type === Items.CPT ? (
+            <ComponentIcon
+              className={`${!selected ? 'fill-blue500' : 'fill-white'}`}
+              width={22}
+              height={22}
             />
           ) : (
-            <div
-              className={`w-2 h-2 rounded-full ${status === sensorStatus.OPR ? 'bg-defaultGreen' : 'bg-defaultRed'}`}
-            ></div>
-          ))}
+            <Image
+              src={`/assets/icons/${type}.svg`}
+              alt="blue color icon"
+              className={`${!selected ? 'fill-white' : 'fill-blue500'}`}
+              width={22}
+              height={22}
+            />
+          )}
+          {name}
+          {isLeaf &&
+            !!sensorType &&
+            (sensorType === sensorTypes.E ? (
+              <EnergyIcon
+                className={`filter ${status === sensorStatus.OPR ? 'fill-defaultGreen' : 'fill-defaultRed'}`}
+              />
+            ) : (
+              <div
+                className={`w-2 h-2 rounded-full ${status === sensorStatus.OPR ? 'bg-defaultGreen' : 'bg-defaultRed'}`}
+              ></div>
+            ))}
+        </div>
+        {isExpanded && children && <div className="pl-4">{children}</div>}
       </div>
-      {isExpanded && children && <div className="pl-4">{children}</div>}
-    </div>
-  );
-};
+    );
+  },
+);
+
+TreeNode.displayName = 'TreeNode';
 
 export default TreeNode;
